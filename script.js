3d00c88a3048f9b961b818fc8dfe0e9afda78fe8
Poornesh-M-D <70961243+Poornesh-M-D@users.noreply.github.com>
2023-01-09 16:59:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let itemInput = document.getElementById('item-name-input');
+let priceInput = document.getElementById('item-price-input');
+let button = document.getElementById('add');
+let table = document.getElementById('table');
+let total = document.getElementById('total');
+
+function addItem(){
+	let item = itemInput.value;
+	let price = priceInput.value;
+
+	if(input!== " " && price!== " ")
+	   {
+			let row = document.createElement('tr');
+		   row.innerHTML = `<td>${item}</td> <td>${price}</td>`
+		   table.append(row);
+		   
+	   }
+}
+
+button.addEventListener('click',addItem)
