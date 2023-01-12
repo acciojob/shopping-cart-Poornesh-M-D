c588388e60bfd777591d121cc5e3345e18875ea3
Poornesh MD <poorneshm.d05@gmail.com>
2023-01-12 10:06:17 +0530

    shopping cart

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your code here
-let itemInput = document.getElementById('item-name-input');
-let priceInput = document.getElementById('item-price-input');
-let button = document.getElementById('add');
-let table = document.getElementById('table');
-let total = document.getElementById('total');
+let itemInput = document.getElementById("item-name-input");
+let priceInput = document.getElementById("item-price-input");
+let button = document.getElementById("add");
+let table = document.getElementById("table");
+let total = document.getElementById("total");
 
 let priceArr = [];
 function addItem() {
@@ -12,17 +12,17 @@ function addItem() {
   let sum = 0;
 
   if (item !== " " && price !== " ") {
-    let row = document.createElement('tr');
+    let row = document.createElement("tr");
     priceArr.push(parseInt(price));
-    row.innerHTML = `<td>${item}</td> <td>${price}</td>`
-    for(let i=0;i<priceArr.length;i++){
-      sum+=priceArr[i];
+    row.innerHTML = `<td>${item}</td> <td>${price}</td>`;
+    for (let i = 0; i < priceArr.length; i++) {
+      sum += priceArr[i];
     }
     table.append(row);
     total.innerText = sum;
-    itemInput.value='';
-    priceInput.value='';
+    itemInput.value = "";
+    priceInput.value = "";
   }
 }
 
-button.addEventListener('click', addItem);
+button.addEventListener("click", addItem);
