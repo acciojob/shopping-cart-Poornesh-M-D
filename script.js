94513a0afe9608721f7daed2fb5aee8078d611c2
Poornesh-M-D <70961243+Poornesh-M-D@users.noreply.github.com>
2023-01-12 08:45:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,28 @@
-var itemName = document.getElementById("item-name-input");
-var itemQuantity = document.getElementById("item-qty-input");
-var itemPrice = document.getElementById("item-price-input");
- 
-var tbody = document.querySelector("tbody");
- 
-var grandTotal = document.getElementById("total");
-var grand_total = 0;
- 
-var addItem = document.querySelector("#add");
- 
-addItem.addEventListener("click", (event) => {
-  event.preventDefault();
- 
-  let newRow = document.createElement("tr");
- 
-  let name = document.createElement("td");
-  name.innerText = itemName.value;
-  name.classList.add("item");
-  newRow.append(name);
- 
-  let price = document.createElement("td");
-  price.innerText = itemPrice.value;
-  newRow.append(price);
-  price.classList.add("price");
- 
-  let quantity = document.createElement("td");
-  quantity.innerText = itemQuantity.value;
-  newRow.append(quantity);
- 
-  if (itemName.value && itemQuantity.value && itemPrice.value) {
-    let total = document.createElement("td");
-    total.innerText = itemPrice.value * itemQuantity.value;
-    newRow.append(total);
- 
-    tbody.append(newRow);
- 
-    grand_total += Number(total.innerText);
-    grandTotal.innerHTML = "Grand Total: ₹ " + grand_total;
+//your code here
+let itemInput = document.getElementById('item-name-input');
+let priceInput = document.getElementById('item-price-input');
+let button = document.getElementById('add');
+let table = document.getElementById('table');
+let total = document.getElementById('total');
+
+let priceArr = [];
+function addItem() {
+  let item = itemInput.value;
+  let price = priceInput.value;
+  let sum = 0;
+
+  if (item !== " " && price !== " ") {
+    let row = document.createElement('tr');
+    priceArr.push(parseInt(price));
+    row.innerHTML = `<td>${item}</td> <td>${price}</td>`
+    for(let i=0;i<priceArr.length;i++){
+      sum+=priceArr[i];
+    }
+    table.append(row);
+    total.innerText = sum;
+    itemInput.value='';
+    priceInput.value='';
   }
- 
-  itemName.value = "";
-  itemQuantity.value = "";
-  itemPrice.value = "";
-});
+}
+
+button.addEventListener('click', addItem);
